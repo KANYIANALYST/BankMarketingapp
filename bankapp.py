1d9750543be132652904016bbb78be1931cc2237
KANYI ESTHER <130997793+KANYIANALYST@users.noreply.github.com>
2024-08-02 12:18:15 +0300

    Update and rename app.py to bankapp.py

--- bankapp.py
+++ bankapp.py
@@ -6,7 +6,7 @@ import seaborn as sns
 # Load data
 @st.cache
 def load_data():
-    url = 'https://raw.githubusercontent.com/KANYIANALYST/BankMarketingapp/main/Copy_of_Bank_Marketing_Prediction.ipynb'
+    url = 'https://raw.githubusercontent.com/KANYIANALYST/BankMarketingapp/main/bank-full.csv'
     data = pd.read_csv(url)
     return data
 
@@ -17,7 +17,8 @@ st.write('## Data Overview')
 st.write(data.head())
 
 # Feature Importance Visualization
-st.write('## Feature Importance')
-fig, ax = plt.subplots()
-sns.barplot(y=data.columns[:-1], x=best_rf.feature_importances_, ax=ax)
-st.pyplot(fig)
+# Uncomment the below lines after defining best_rf in your code
+# st.write('## Feature Importance')
+# fig, ax = plt.subplots()
+# sns.barplot(y=data.columns[:-1], x=best_rf.feature_importances_, ax=ax)
+# st.pyplot(fig)
