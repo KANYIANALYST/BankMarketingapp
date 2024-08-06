4a6670d41e6beca6a3300487aab7e6515a507b38
KANYI ESTHER <130997793+KANYIANALYST@users.noreply.github.com>
2024-08-06 15:08:48 +0300

    Update bankapp.py

--- bankapp.py
+++ bankapp.py
@@ -6,21 +6,21 @@ import joblib
 from urllib.request import urlopen
 
 # Load data
-@st.cache
+@st.cache_data
 def load_data():
     url = 'https://raw.githubusercontent.com/KANYIANALYST/BankMarketingapp/main/bank-full-encoded.csv'
     data = pd.read_csv(url)
     return data
 
 # Load feature importance
-@st.cache
+@st.cache_data
 def load_feature_importance():
     url = 'https://raw.githubusercontent.com/KANYIANALYST/BankMarketingapp/main/feature_importance.csv'
     feature_importance_df = pd.read_csv(url)
     return feature_importance_df
 
 # Load model
-@st.cache
+@st.cache_data
 def load_model():
     url = 'https://github.com/KANYIANALYST/BankMarketingapp/blob/main/best_rf_model.pkl?raw=true'
     model = joblib.load(urlopen(url))
@@ -34,15 +34,16 @@ st.title('Bank Marketing Campaign Analysis')
 st.write('## Data Overview')
 st.write(data.head())
 
-# Ensure the feature importances length matches the number of features
-num_features = len(data.columns) - 1  # Exclude the target variable column
-feature_importances_length = len(feature_importance_df)
+# Exclude the target variable from the dataset columns
+target_variable = 'y'  # Replace with the actual name of your target variable
+data_features = [col for col in data.columns if col != target_variable]
 
-if num_features != feature_importances_length:
-    st.error(f"Mismatch in lengths: Number of features in dataset ({num_features}) does not match length of feature importances ({feature_importances_length}).")
+# Check for mismatch
+if len(data_features) != len(feature_importance_df):
+    st.error(f"Mismatch in lengths: Number of features in dataset ({len(data_features)}) does not match length of feature importances ({len(feature_importance_df)}).")
 else:
     # Feature Importance Visualization
     st.write('## Feature Importance')
     fig, ax = plt.subplots()
-    sns.barplot(y=data.columns[:-1], x=feature_importance_df['importance'], ax=ax)
+    sns.barplot(y=data_features, x=feature_importance_df['importance'], ax=ax)
     st.pyplot(fig)
