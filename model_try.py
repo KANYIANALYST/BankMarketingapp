134b1a15e3f49affa52ac1fecdec53cd8c38d754
KANYI ESTHER <130997793+KANYIANALYST@users.noreply.github.com>
2024-08-13 15:04:49 +0300

    Update model_try.py

--- model_try.py
+++ model_try.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 import joblib
+from urllib.request import urlopen
 
 # Load model
 @st.cache_data
